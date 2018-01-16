85a375b19d89dbb094b0b9b6d26517862de38ffe
nikolovjd <nikolov.jd@gmail.com>
2018-01-16 13:39:11 +0100

    created config

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const JanusPlugin = require('./src/JanusPlugin')
 const EchoJanusPlugin = require('./src/plugin/EchoJanusPlugin')
 const VideoRoomListenerJanusPlugin = require('./src/plugin/VideoRoomListenerJanusPlugin')
 const VideoRoomPublisherJanusPlugin = require('./src/plugin/VideoRoomPublisherJanusPlugin')
+const { JanusConfig } = require('./src/Config')
+const { JanusAdminConfig } = require('./src/Config')
+
 
 module.exports.Janus = Janus
 module.exports.JanusAdmin = JanusAdmin
@@ -11,3 +14,5 @@ module.exports.JanusPlugin = JanusPlugin
 module.exports.EchoJanusPlugin = EchoJanusPlugin
 module.exports.VideoRoomListenerJanusPlugin = VideoRoomListenerJanusPlugin
 module.exports.VideoRoomPublisherJanusPlugin = VideoRoomPublisherJanusPlugin
+module.exports.JanusConfig = JanusConfig
+module.exports.JanusAdminConfig = JanusAdminConfig
