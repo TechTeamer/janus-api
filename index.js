e1efbc19897d00c626108310ec0d8f755f7f7947
Gábor Tóth <tgabi333@gmail.com>
2018-01-27 18:14:13 +0100

    export RecordPlayJanusPlugin

--- index.js
+++ index.js
@@ -2,16 +2,19 @@ const Janus = require('./src/Janus')
 const JanusAdmin = require('./src/JanusAdmin')
 const JanusPlugin = require('./src/JanusPlugin')
 const EchoJanusPlugin = require('./src/plugin/EchoJanusPlugin')
+const RecordPlayJanusPlugin = require('./src/plugin/RecordPlayJanusPlugin')
 const VideoRoomListenerJanusPlugin = require('./src/plugin/VideoRoomListenerJanusPlugin')
 const VideoRoomPublisherJanusPlugin = require('./src/plugin/VideoRoomPublisherJanusPlugin')
-const { JanusConfig } = require('./src/Config')
-const { JanusAdminConfig } = require('./src/Config')
+const { JanusConfig, JanusAdminConfig } = require('./src/Config')
 
-module.exports.Janus = Janus
-module.exports.JanusAdmin = JanusAdmin
-module.exports.JanusPlugin = JanusPlugin
-module.exports.EchoJanusPlugin = EchoJanusPlugin
-module.exports.VideoRoomListenerJanusPlugin = VideoRoomListenerJanusPlugin
-module.exports.VideoRoomPublisherJanusPlugin = VideoRoomPublisherJanusPlugin
-module.exports.JanusConfig = JanusConfig
-module.exports.JanusAdminConfig = JanusAdminConfig
+module.exports = {
+  Janus,
+  JanusAdmin,
+  JanusPlugin,
+  EchoJanusPlugin,
+  RecordPlayJanusPlugin,
+  VideoRoomListenerJanusPlugin,
+  VideoRoomPublisherJanusPlugin,
+  JanusConfig,
+  JanusAdminConfig
+}
