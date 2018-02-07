45f32c0a50bdf3f24a5ec7c4f6717076300c80fb
tbence94 <tihanyibence@gmail.com>
2018-02-07 15:54:41 +0100

    changes

--- test/videoroom/listener.js
+++ test/videoroom/listener.js
@@ -72,12 +72,12 @@ function connect () {
           peerConnection.setLocalDescription(answer)
           listener.setAnswer(answer)
         })
+      })
 
-        listener.on('hangup', () => {
-          let videoElement = document.getElementById('video')
-          videoElement.pause()
-          videoElement.srcObject = null
-        })
+      listener.on('hangup', () => {
+        let videoElement = document.getElementById('video')
+        videoElement.pause()
+        videoElement.srcObject = null
       })
 
       return listener.join()
