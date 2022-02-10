98af942c7b636a566fbcc617944858f520f1a06a
jurki <www.jurki@gmail.com>
2022-02-10 13:13:10 +0100

    FKDEV-29: Talking event (#29)

--- src/plugin/VideoRoomPublisherJanusPlugin.js
+++ src/plugin/VideoRoomPublisherJanusPlugin.js
@@ -285,6 +285,16 @@ class VideoRoomPublisherJanusPlugin extends JanusPlugin {
       return
     }
 
+    if (videoroom === 'talking') {
+      this.emit('talking')
+      return
+    }
+
+    if (videoroom === 'stopped-talking') {
+      this.emit('stoppedTalking')
+      return
+    }
+
     this.logger.error('VideoRoomPublisherJanusPlugin unhandled message:', videoroom, json)
   }
 }
